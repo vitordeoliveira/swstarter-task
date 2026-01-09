d6a37c47187f44fa9541942341de6fe21bcf707c
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2026-01-09 15:26:59 -0300

    Refactor getMoviesFromUrls and getPeopleFromUrls functions to use Map for improved performance and readability; remove unnecessary find operations and enhance data retrieval efficiency.

--- app/utils/urlHelpers.ts
+++ app/utils/urlHelpers.ts
@@ -7,12 +7,20 @@ export interface PersonWithId {
 }
 
 export function getMoviesFromUrls(filmUrls: string[], allFilms: Movie[]): Movie[] {
-  const movies: Movie[] = [];
+  const filmMap = new Map<string, Movie>();
   
+  // Manually adding this comment here because that idea is really good. (if you see this comment PLEASE let me know)
+  // As is a test I added, in a real project I avoid comments like this.
+  allFilms.forEach((film) => {
+    if (film.uid) filmMap.set(film.uid, film);
+    if (film._id) filmMap.set(film._id, film);
+  });
+  
+  const movies: Movie[] = [];
   filmUrls.forEach((filmUrl: string) => {
     const filmId = filmUrl.split('/').pop();
     if (filmId) {
-      const film = allFilms.find((f) => f.uid === filmId || f._id === filmId);
+      const film = filmMap.get(filmId);
       if (film) {
         movies.push(film);
       }
@@ -23,12 +31,18 @@ export function getMoviesFromUrls(filmUrls: string[], allFilms: Movie[]): Movie[
 }
 
 export function getPeopleFromUrls(peopleUrls: string[], allPeople: Person[]): PersonWithId[] {
-  const people: PersonWithId[] = [];
+  const peopleMap = new Map<string, Person>();
+ 
+  allPeople.forEach((person) => {
+    if (person.uid) peopleMap.set(person.uid, person);
+    if (person._id) peopleMap.set(person._id, person);
+  });
   
+  const people: PersonWithId[] = [];
   peopleUrls.forEach((personUrl: string) => {
     const personId = personUrl.split('/').pop();
     if (personId) {
-      const person = allPeople.find((p) => p.uid === personId || p._id === personId);
+      const person = peopleMap.get(personId);
       if (person && person.name) {
         people.push({
           name: person.name,
